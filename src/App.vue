2e2f6d3b57484a4db6a54fc4cf410753b2e34c62
Xian704011 <a0984030252@gmail.com>
2023-05-30 19:53:07 +0800

    finish

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Page6000 from './components/Page6000.vue'
+</script>
+
+<template>
+
+
+  <main>
+    <Page6000 />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
